ad8a6e556b4e33a1c311b16ba8a84c474d550c75
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 21:24:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ function accessElementInArray(arr, idx) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(arr) {
-  arr.shift()
-  return arr
+  return arr.shift()
+  // return arr
 }
