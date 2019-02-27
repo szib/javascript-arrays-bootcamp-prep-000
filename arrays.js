dedb8d1151f68700d472e45c6fff21f88ecff43a
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 21:19:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ function destructivelyAddElementToBeginningOfArray(arr, element) {
   arr.unshift(element)
   return arr
 }
+
+function addElementToBeginningOfArray(arr, element) {
+  return [element, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  arr.unshift(element)
+  return arr
+}
